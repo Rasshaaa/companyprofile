05e9ee39c0c51c726cff9aec82f28dfb7814e905
Rasshaaa <maulanarasya264@gmail.com>
2023-12-16 23:49:06 +0700

    About Section in progress

--- company.js
+++ company.js
@@ -1,1 +1,14 @@
+// Variables for the most outside
+const nav = document.querySelector('.navbar');
+
+// Rellax Setup
 var rellax = new Rellax('.rellax');
+
+// Fixed Navbar
+document.addEventListener('scroll', (e) => {
+    if (window.scrollY > window.innerHeight) {
+        nav.classList.add('fixed');
+    } else {
+        nav.classList.remove('fixed');
+    }
+});
