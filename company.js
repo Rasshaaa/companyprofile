0e3be205d705c275d58fa581b404d6bb230cffb9
Rasshaaa <maulanarasya264@gmail.com>
2023-12-28 21:52:23 +0700

    beautify with AOS

--- company.js
+++ company.js
@@ -12,3 +12,33 @@ document.addEventListener('scroll', (e) => {
         nav.classList.remove('transparent');
     }
 });
+
+const rootElement = document.querySelector(":root");
+
+// Disable Scroll via add class list
+function disableScroll() {
+    document.body.classList.add("stop-scrolling");
+}
+
+// Agar disable scroll saat posisi scroll berada pada titik 0 (teratas)
+if(window.scrollY == 0) {
+    disableScroll();
+}
+
+// Agar saat reload page di section lain auto pergi ke section awal
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+
+// Enable Scroll via add class list
+function enableScroll() { 
+    document.body.classList.remove("stop-scrolling");
+    rootElement.style.scrollBehavior = 'smooth';
+}
+
+// Initialize AOS
+AOS.init();
